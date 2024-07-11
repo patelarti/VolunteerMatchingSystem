a649cfcdb84c0ee611a2cf157185d6696475a54b
Arti <patelarti91@gmail.com>
2024-07-10 21:57:38 -0500

    update the code for notification module

--- app/notifications/routes.py
+++ app/notifications/routes.py
@@ -1,8 +1,19 @@
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 notifications_bp = Blueprint('notification', __name__)
 
 @notifications_bp.route('/')
 def notification():
-    return render_template('notification.html')
+    notifications = [
+        "You have a new message",
+        "Volunteer matched successfully",
+        "Your event was approved",
+        "Indicates a warning that might need attention",
+        "Your event was approved",
+        "Indicates a warning that might need attention",
+        "Your event was approved",
+        "Indicates a warning that might need attention"
+    ]
+
+    return render_template('notification.html', notifications = notifications)
