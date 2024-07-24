70251631174a9c4b1a07a03b37868f7fd02bcca0
patelarti <patelarti91@gmail.com>
2024-07-24 18:07:53 -0500

    fixed user profile (fixed duplication issue), volunteer matching (now fully connected to db).

--- app/user_profile/routes.py
+++ app/user_profile/routes.py
@@ -14,6 +14,7 @@ conn = psycopg2.connect(database="volunteers_db", user="postgres",
 def profile():
     if session.get('signed_in') is None or session["signed_in"] == False:
         return render_template("index.html")
+
     if request.method == 'POST':
         data = request.get_json()
         email = data.get('email')
@@ -29,23 +30,38 @@ def profile():
         availability = data.get('availability')
 
         formatted_skills = ""
-        for skill in skills:
-            formatted_skills += skill + ","
+        for i, skill in enumerate(skills):
+            formatted_skills += skill + ("," if i < len(skills) - 1 else "")
 
-        print(f"profile data==>{data}")
+        # print(f"profile data==>{data}")
 
         formatted_availability_date = "".join(str(availability).split('-'))
+
+        # check if there's already an entry in the user_profile table.
+        # if yes: use UPDATE
+        # else: use INSERT
+
         cursor = conn.cursor()
 
-        command = f'''INSERT INTO user_profile(user_id, full_name, address_1, address_2, city, state, zipcode, skills, preference, availability)
-                    VALUES({session["user_id"]}, '{full_name}', '{address1}', '{address2}', '{city}', '{state}', '{zip_code}', '{formatted_skills}', '{preferences}', date({formatted_availability_date}::TEXT));'''
+        command = f'''SELECT user_id FROM user_profile WHERE user_id = {session['user_id']};'''
+        cursor.execute(command)
+        table_data = cursor.fetchone()
+
+        if table_data:  # profile row already exists
+            command = f'''UPDATE user_profile
+                          SET full_name = '{full_name}', address_1 = '{address1}', address_2 = '{address2}', 
+                              city = '{city}', state = '{state}', zipcode = '{zip_code}', skills = '{formatted_skills}', 
+                              preference = '{preferences}', availability = '{formatted_availability_date}'
+                          WHERE user_id = {session['user_id']}'''
+        else:
+            command = f'''INSERT INTO user_profile(user_id, full_name, address_1, address_2, city, state, zipcode, skills, preference, availability)
+                        VALUES({session["user_id"]}, '{full_name}', '{address1}', '{address2}', '{city}', '{state}', '{zip_code}', '{formatted_skills}', '{preferences}', date({formatted_availability_date}::TEXT));'''
 
         print(command)
         cursor.execute(command)
-        # db_password = cursor.fetchone()
         cursor.close()
         conn.commit()
-        conn.close()
+        # conn.close()
 
         # user = next((user for user in testUsers if user['email'] == email), None)
         # if not user:
@@ -70,6 +86,7 @@ def profile():
     # elif request.method == "GET":
 
     # print(session['user_id'])
+    # print(f"session[userid] {session['user_id']}")
     cursor = conn.cursor()
     command = f"SELECT *\
                 FROM user_profile\
@@ -94,3 +111,102 @@ def profile():
     return render_template('profile.html', username=session['username'], full_name=full_name,
                            preferences=preferences, address1=address1, address2=address2, city=city, state=state, zipcode=zipcode, availability=availability, skills=skills)
 
+
+
+### old code.
+# from flask import render_template, request, jsonify, session, Blueprint
+# # from app.user_profile import profile_bp
+# import psycopg2
+# import bcrypt
+#
+# from app.auth.routes import testUsers  # Assuming testUsers is defined in app.auth.routes
+# profile_bp = Blueprint('profile', __name__)
+#
+# # Connect to the database
+# conn = psycopg2.connect(database="volunteers_db", user="postgres",
+#                         password="arti", host="localhost", port="5432")
+#
+# @profile_bp.route('/', methods=['GET', 'POST'])
+# def profile():
+#     if session.get('signed_in') is None or session["signed_in"] == False:
+#         return render_template("index.html")
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         email = data.get('email')
+#         full_name = data.get('fullName')
+#         dob = data.get('dob')
+#         address1 = data.get('address1')
+#         address2 = data.get('address2')
+#         city = data.get('city')
+#         state = data.get('state')
+#         zip_code = data.get('zip')
+#         skills = data.get('skills')
+#         preferences = data.get('preferences')
+#         availability = data.get('availability')
+#
+#         formatted_skills = ""
+#         for skill in skills:
+#             formatted_skills += skill + ","
+#
+#         print(f"profile data==>{data}")
+#
+#         formatted_availability_date = "".join(str(availability).split('-'))
+#         cursor = conn.cursor()
+#
+#         command = f'''INSERT INTO user_profile(user_id, full_name, address_1, address_2, city, state, zipcode, skills, preference, availability)
+#                     VALUES({session["user_id"]}, '{full_name}', '{address1}', '{address2}', '{city}', '{state}', '{zip_code}', '{formatted_skills}', '{preferences}', date({formatted_availability_date}::TEXT));'''
+#
+#         print(command)
+#         cursor.execute(command)
+#         # db_password = cursor.fetchone()
+#         cursor.close()
+#         conn.commit()
+#         conn.close()
+#
+#         # user = next((user for user in testUsers if user['email'] == email), None)
+#         # if not user:
+#         #     return jsonify({'message': 'User not found'}), 404
+#         #
+#         # user.update({
+#         #     'fullName': full_name,
+#         #     'dob': dob,
+#         #     'address1': address1,
+#         #     'address2': address2,
+#         #     'city': city,
+#         #     'state': state,
+#         #     'zip': zip_code,
+#         #     'skills': skills,
+#         #     'preferences': preferences,
+#         #     'availability': availability
+#         # })
+#
+#         return jsonify({'message': 'Profile updated successfully'}), 200
+#
+#     # return render_template('profile.html', username=session['username'])
+#     # elif request.method == "GET":
+#
+#     # print(session['user_id'])
+#     cursor = conn.cursor()
+#     command = f"SELECT *\
+#                 FROM user_profile\
+#                 WHERE user_id = '{session['user_id']}';"
+#     cursor.execute(command)
+#     table_data = cursor.fetchone()
+#     cursor.close()
+#
+#     if table_data:
+#         full_name = table_data[1]
+#         address1, address2 = table_data[2], table_data[3]
+#         city, state, zipcode = table_data[4], table_data[5], table_data[6]
+#         skills, preferences, availability = table_data[7], table_data[8], table_data[9]
+#         skills = skills.split(',')
+#         print("skills==>", skills)
+#     else:
+#         full_name = ""
+#         address1, address2 = "", ""
+#         city, state, zipcode = "", "Select your state", ""
+#         skills, preferences, availability = [], "", ""
+#
+#     return render_template('profile.html', username=session['username'], full_name=full_name,
+#                            preferences=preferences, address1=address1, address2=address2, city=city, state=state, zipcode=zipcode, availability=availability, skills=skills)
+#
--- app/volunteer_matching/routes.py
+++ app/volunteer_matching/routes.py
@@ -1,5 +1,5 @@
+# noinspection PyInterpreter
 from flask import Blueprint, render_template, jsonify, request, session
-from .data import events, volunteers
 import psycopg2
 from .models import Volunteer, Event
 
@@ -8,12 +8,16 @@ matching_bp = Blueprint('matching', __name__)
 # Connect to the database
 conn = psycopg2.connect(database="volunteers_db", user="postgres",
                         password="arti", host="localhost", port="5432")
+
+events = []
+volunteers = []
 @matching_bp.route('/', methods=['GET'])
 def volunteer_matching():
     if session.get('signed_in') is None or session["signed_in"] == False:
         return render_template("index.html")
 
     return render_template('volunteer_matching.html', username=session['username'])
+
 
 @matching_bp.route('/api/volunteers', methods=['GET'])
 def get_volunteers():
@@ -22,11 +26,65 @@ def get_volunteers():
     command = f"SELECT * FROM user_profile;"
 
     cursor.execute(command)
-    db_id = cursor.fetchone()
-    print("user_profile table data (from volunteer matching):", db_id)
+    table_data = cursor.fetchall()
+    # print("user_profile table data (from volunteer matching):", table_data)
+    # cursor.close()
+
+    # create Volunteer objects and store in volunteer list
+    for row in table_data:
+        # fetch this user's email
+        command = f"SELECT email FROM usercredentials WHERE id = {row[0]};"
+        cursor.execute(command)
+        email = cursor.fetchone()[0]
+
+        # in the same session, it adds the volunteers as many times as you refresh, hence this check.
+        exists = False
+        record_idx = -1
+        for volunteer in volunteers:
+            record_idx += 1
+            # print(f"vol.email => {volunteer.to_dict()['email']}, email => {email}, equality => {volunteer.to_dict()['email'] == email}")
+            # this is O(n). Can we improve this?
+            # volunteer below is a new object hence "if volunteer in volunteers" isn't working.
+            if volunteer.to_dict()['email'] == email:
+                exists = True
+                break
+
+        # Fetch volunteer's history.
+        command = f"SELECT event_id FROM volunteer_history WHERE user_id = {row[0]};"
+        cursor.execute(command)
+        history = []
+        table_data = cursor.fetchall()
+
+        for hist in table_data:
+            history.append({"event_id": hist[0], "status": "Incomplete"})
+        # End volunteer history
+
+        formatted_skills = row[7].split(',')
+        volunteer = Volunteer(
+            name=row[1],
+            address1=row[2],
+            address2=row[3],
+            city=row[4],
+            state=row[5],
+            zip=row[6],
+            skills=formatted_skills,
+            preferences=row[8],
+            availability=[row[9]],
+            email=email,
+            phone="PHONE DUMMY DATA",
+            history=history
+        )
+        if exists:
+            # the history may have been updated, hence replace
+            volunteers[record_idx] = volunteer
+        else:
+            volunteers.append(volunteer)
     cursor.close()
+    # for volunteer in volunteers:
+    #     print(volunteer.to_dict())
 
     return jsonify([volunteer.to_dict() for volunteer in volunteers])
+
 
 @matching_bp.route('/api/events', methods=['GET'])
 def get_events():
@@ -35,35 +93,65 @@ def get_events():
     command = f"SELECT * FROM event_details;"
 
     cursor.execute(command)
-    db_id = cursor.fetchone()
-    print("event_details table data (from volunteer matching):", db_id)
+    table_data = cursor.fetchall()
+    print("event_details table data (from volunteer matching):", table_data)
     cursor.close()
 
+    # create Event objects and store in event list
+    for row in table_data:
+        # in the same session, it adds the events as many times as you refresh, hence this check.
+        exists = False
+        for event in events:
+            if event.to_dict()['id'] == row[0]:
+                exists = True
+                break
+
+        if exists:
+            continue
+
+        formatted_required_skills = row[4].split(",")
+        event = Event(
+            name=row[1],
+            description=row[2],
+            location=row[3],
+            required_skills=formatted_required_skills,
+            urgency=row[5],
+            date=row[6],
+            id=row[0],
+        )
+        events.append(event)
+
     return jsonify([event.to_dict() for event in events])
+
 
 @matching_bp.route('/api/assign_event', methods=['POST'])
 def assign_event():
     data = request.get_json()
     volunteer_name = data.get('volunteer_name')
     event_name = data.get('event_name')
-    # cursor = conn.cursor()
+    cursor = conn.cursor()
 
-    # # get user_id using volunteer_name
-    # command = f'''SELECT user_id FROM user_profile WHERE full_name = {volunteer_name};'''
-    # cursor.execute(command)
-    # user_id = cursor.fetchone()[0]
-    #
-    # # get event_id from event_name
-    # command = f'''SELECT event_id FROM event_details WHERE event_name = {event_name}'''
-    # cursor.execute(command)
-    # event_id = cursor.fetchone()[0]
-    #
-    # # create a new record in history_db
-    # command = f'''INSERT INTO volunteer_history(user_id, event_id) VALUES ({user_id}, {event_id});'''
-    # cursor.execute(command)
-    # cursor.close()
-    # conn.commit()
+    # volunteer_name_db = 'Arti Patel'
+    # get user_id using volunteer_name
+    command = f'''SELECT user_id FROM user_profile WHERE full_name = '{volunteer_name}';'''
+    cursor.execute(command)
+    user_id = cursor.fetchone()[0]
 
+    # event_name_db = 'Hiking'
+    # get event_id from event_name
+    command = f'''SELECT event_id FROM event_details WHERE event_name = '{event_name}';'''
+    cursor.execute(command)
+    event_id = cursor.fetchone()[0]
+
+    # print(f"user_id = {user_id}, event_id = {event_id}")
+
+    # create a new record in history_db
+    command = f'''INSERT INTO volunteer_history(user_id, event_id) VALUES ('{user_id}', '{event_id}');'''
+    cursor.execute(command)
+    cursor.close()
+    conn.commit()
+
+    # we will have already created Volunteer and Event objects and stored them. Simply check them here.
     for volunteer in volunteers:
         if volunteer.name == volunteer_name:
             volunteer.assigned_event = event_name
@@ -73,3 +161,78 @@ def assign_event():
 
 
 
+# from flask import Blueprint, render_template, jsonify, request, session
+# from .data import events, volunteers
+# import psycopg2
+# from .models import Volunteer, Event
+#
+# matching_bp = Blueprint('matching', __name__)
+#
+# # Connect to the database
+# conn = psycopg2.connect(database="volunteers_db", user="postgres",
+#                         password="arti", host="localhost", port="5432")
+# @matching_bp.route('/', methods=['GET'])
+# def volunteer_matching():
+#     if session.get('signed_in') is None or session["signed_in"] == False:
+#         return render_template("index.html")
+#
+#     return render_template('volunteer_matching.html', username=session['username'])
+#
+# @matching_bp.route('/api/volunteers', methods=['GET'])
+# def get_volunteers():
+#     cursor = conn.cursor()
+#
+#     command = f"SELECT * FROM user_profile;"
+#
+#     cursor.execute(command)
+#     db_id = cursor.fetchone()
+#     print("user_profile table data (from volunteer matching):", db_id)
+#     cursor.close()
+#
+#     return jsonify([volunteer.to_dict() for volunteer in volunteers])
+#
+# @matching_bp.route('/api/events', methods=['GET'])
+# def get_events():
+#     cursor = conn.cursor()
+#
+#     command = f"SELECT * FROM event_details;"
+#
+#     cursor.execute(command)
+#     db_id = cursor.fetchone()
+#     print("event_details table data (from volunteer matching):", db_id)
+#     cursor.close()
+#
+#     return jsonify([event.to_dict() for event in events])
+#
+# @matching_bp.route('/api/assign_event', methods=['POST'])
+# def assign_event():
+#     data = request.get_json()
+#     volunteer_name = data.get('volunteer_name')
+#     event_name = data.get('event_name')
+#     # cursor = conn.cursor()
+#
+#     # # get user_id using volunteer_name
+#     # command = f'''SELECT user_id FROM user_profile WHERE full_name = {volunteer_name};'''
+#     # cursor.execute(command)
+#     # user_id = cursor.fetchone()[0]
+#     #
+#     # # get event_id from event_name
+#     # command = f'''SELECT event_id FROM event_details WHERE event_name = {event_name}'''
+#     # cursor.execute(command)
+#     # event_id = cursor.fetchone()[0]
+#     #
+#     # # create a new record in history_db
+#     # command = f'''INSERT INTO volunteer_history(user_id, event_id) VALUES ({user_id}, {event_id});'''
+#     # cursor.execute(command)
+#     # cursor.close()
+#     # conn.commit()
+#
+#     for volunteer in volunteers:
+#         if volunteer.name == volunteer_name:
+#             volunteer.assigned_event = event_name
+#             return jsonify({"message": f"Event '{event_name}' assigned to volunteer '{volunteer_name}'"}), 200
+#
+#     return jsonify({"error": "Volunteer not found"}), 404
+#
+#
+#
